406afb645c4d16cab54baa0a13c3919061047a8a
Иван Заигрин <ivanzaigrin@gmail.com>
2026-03-17 05:25:45 +0300

    Add classes: Sale and SaleManager

---
+++ SaleApp/Sale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SalesApp
+{
+    public class Sale
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Date { get; set; }
+
+        public Sale(string productName, decimal price, int quantity, DateTime date)
+        {
+            ProductName = productName;
+            Price = price;
+            Quantity = quantity;
+            Date = date;
+        }
+
+        public decimal TotalRevenue
+        {
+            get { return Price * Quantity; }
+        }
+    }
+}
